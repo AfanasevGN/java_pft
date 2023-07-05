264926b296664f28776d34d6d2c45758250ed132
Gleb <siglebson@gmail.com>
2023-07-05 19:09:58 +0300

    Doing appleinsider.ru test

---
+++ YoutubeProject/src/test/java/MainPage.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+/** главная страница сайта appleinsider.ru
+*
+ */
+public class MainPage {
+    private final SelenideElement textBoxInput = $x("//input[@type='text']");
+
+    public MainPage(String url){
+        Selenide.open(url);
+    }
+
+    /**
+     * Выполняется поиск на сайте среди статей и нажимается кнопка Enter
+     * @param searchString поисковая строка
+     */
+    public void search(String searchString){
+        textBoxInput.setValue(searchString);
+        textBoxInput.sendKeys(Keys.ENTER);
+
+    }
+}
